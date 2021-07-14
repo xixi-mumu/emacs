a0df5543aeea99b01a786d923f13823cb47708d7
ptshe <peitong.she@ingenic.com>
2021-07-15 00:15:01 +0800

    init my emacs init config files

---
+++ xixi-mumu-emacs-config/.emacs.d/my-macro/my-macro.el
@@ -0,0 +1,26 @@
+;;; Made by spt
+
+;;; Code:
+(fset 'up-spt
+   [up up up up up up up up up up up up up up up])
+(fset 'down-spt
+   [down down down down down down down down down down down down down down down])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;;; my-macro.el ends here
---
+++ xixi-mumu-emacs-config/.emacs.d/plugins/matlab-emacs/aaa.m
@@ -0,0 +1,16 @@
+%{
+
+aaaa
+
+%}
+
+if (1)
+    a = 12;
+
+else
+    a = 8;
+
+end
+
+fp = fopen('aaaaa','rb');
+fclose(fp);
